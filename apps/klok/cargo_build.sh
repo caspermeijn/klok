2d28c25c4396d215a0a4b2ebb8e68df95a7ef52b
Casper Meijn <casper@meijn.net>
2020-10-23 09:50:57 +0200

    build: Add new target klok-pinetime-release

--- apps/klok/cargo_build.sh
+++ apps/klok/cargo_build.sh
@@ -5,5 +5,11 @@ set -eu
 # SPDX-License-Identifier: CC0-1.0
 
 TARGET="thumbv7m-none-eabi"
-cargo +nightly build --target="${TARGET}" --target-dir="${MYNEWT_PKG_BIN_DIR}"
-cp "${MYNEWT_PKG_BIN_DIR}"/${TARGET}/debug/*.a "${MYNEWT_PKG_BIN_ARCHIVE}"
+
+if [ "${MYNEWT_BUILD_PROFILE}" = "optimized" ] || [ "${MYNEWT_BUILD_PROFILE}" = "speed" ]; then
+  cargo +nightly build --release --target="${TARGET}" --target-dir="${MYNEWT_PKG_BIN_DIR}"
+  cp "${MYNEWT_PKG_BIN_DIR}"/${TARGET}/release/*.a "${MYNEWT_PKG_BIN_ARCHIVE}"
+else
+  cargo +nightly build --target="${TARGET}" --target-dir="${MYNEWT_PKG_BIN_DIR}"
+  cp "${MYNEWT_PKG_BIN_DIR}"/${TARGET}/debug/*.a "${MYNEWT_PKG_BIN_ARCHIVE}"
+fi
